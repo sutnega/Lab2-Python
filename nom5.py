f197e4dc56a0fc2c4f2a5c0ae449bfc4a3d1e590
sutnega <104433317+sutnega@users.noreply.github.com>
2022-10-28 20:29:45 +0300

    Add files via upload

---
+++ nom5.py
@@ -0,0 +1,9 @@
+def level5(a):
+    if a % 2 == 0:
+        return a == 2
+    d = 3
+    while d * d <= a and a % d != 0:
+        d += 2
+    return d * d > a
+
+print(level5(int(input("Введите число: "))))
